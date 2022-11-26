8e1410c644196ef3bbc35a4ee56151f401402e95
Mariutti <pedromariutti@gmail.com>
2022-11-26 11:33:59 -0300

    Giving comments to explain my algorithm

--- javascript.js
+++ javascript.js
@@ -8,10 +8,9 @@ function getComputerChoice(){
 }
 
 
-// creating a var where we kept the player choice:
+// creating a var where we kept the player choice. I used a function to return the result:
 function choseOne() {
-    const playerChoice = window.prompt("rock, paper or scissors!").toLowerCase();
-    return playerChoice;
+    return window.prompt("rock, paper or scissors!").toLowerCase();
 }
 
 
@@ -20,6 +19,9 @@ function playRound(playerSelection, computerSelection){
     console.log("Computer gets " + computerSelection)
     console.log("You chose " + playerSelection);
 
+    //analyzing all results 
+    //showing a message with the round result
+    //returning a single word with the result
     switch (playerSelection) {
         case "rock":
             switch (computerSelection) {
@@ -87,16 +89,21 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-
+// creating a function to play a game with five round
 function game(){
+    //creating a var to count the rounds
     let rounds = 1;
+    //creating a var to count the player points
     let playerPoints = 0;
+    //creating a ver to count the computer points
     let computerPoints = 0;
-    for (let i = 0; i < 5; i++){
-        
-        let gameResult = playRound(choseOne(), getComputerChoice());
-        // console.log(gameResult)
 
+    // creating a loop of five rounds to play the function playRound
+    for (let i = 0; i < 5; i++){
+        // creating a var to keep the result o the function playRound()
+        let gameResult = playRound(choseOne(), getComputerChoice());
+        
+        //giving the points of the winner
         switch (gameResult) {
             case "player":
                 playerPoints += 1;
@@ -109,10 +116,11 @@ function game(){
             default:
                 break;
         }
-
+        //showing the round number and the result of rounds till there
         console.log("Round "+ rounds +" results: Player: " + playerPoints + " ---- Computer: " + computerPoints);
         rounds++;
 
+        //it's a five round game, so if someone gets 3 points, we have a winner and don't need to play to the end of rounds
         if (playerPoints == 3){
             return window.alert("You won 3 rounds and you're the winner!!!")
         } else if (computerPoints == 3) {
@@ -120,6 +128,7 @@ function game(){
         }
     }
 
+    //if nobody gets 3 points till the end of loop, we will compare the results do declare the winner (or tie,)
     if (playerPoints > computerPoints){
         return window.alert("You won "+ playerPoints +" rounds and you're the winner!!!")
     } else if (playerPoints < computerPoints){
