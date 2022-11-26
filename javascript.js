53e5edccc6faee641d186f1f40abcfdbf90a4bec
Mariutti <pedromariutti@gmail.com>
2022-11-26 11:04:19 -0300

    Creating the function game() to get 5 rounds of a single game

--- javascript.js
+++ javascript.js
@@ -6,26 +6,34 @@ function getComputerChoice(){
     const gameOptions = ["rock", "paper", "scissors"]
     return gameOptions[Math.floor(Math.random()*3)]
 }
-console.log(getComputerChoice())
 
-// creaating a var where we kept the player choice:
-const playerChoice = window.prompt("rock, paper or scissors!").toLowerCase();
-console.log(playerChoice);
+
+// creating a var where we kept the player choice:
+function choseOne() {
+    const playerChoice = window.prompt("rock, paper or scissors!").toLowerCase();
+    return playerChoice;
+}
 
 
 // creating a function to play a single round of the game
-function playOneRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){
+    console.log("Computer gets " + computerSelection)
+    console.log("You chose " + playerSelection);
+
     switch (playerSelection) {
         case "rock":
             switch (computerSelection) {
                 case "rock":
-                    return "There's a tie! Nobody wins!"
+                    console.log("It's a tie! Nobody wins!");
+                    return "tie"
                     break;
                 case "paper":
-                    return "You Lose! "+ computerSelection+" beats " + playerSelection +" !"
+                    console.log("You Lose! "+ computerSelection+" beats " + playerSelection +" !");
+                    return "computer"
                     break;
                 case "scissors":
-                    return "You WIN!!! " + playerSelection +" beats " +  computerSelection+ " !!!"
+                    console.log("You WIN!!! " + playerSelection +" beats " +  computerSelection+ " !!!");
+                    return "player"
                     break;
             
                 default:
@@ -36,13 +44,16 @@ function playOneRound(playerSelection, computerSelection){
         case "paper":
             switch (computerSelection) {
                 case "rock":
-                    return "You WIN!!! " + playerSelection +" beats " +  computerSelection+ " !!!"
+                    console.log("You WIN!!! " + playerSelection +" beats " +  computerSelection+ " !!!");
+                    return "player"
                     break;
-                case "paper":
-                    return "There's a tie! Nobody wins!"
+                case "paper": 
+                    console.log("It's a tie! Nobody wins!");
+                    return "tie"
                     break;
                 case "scissors":
-                    return "You Lose! "+ computerSelection+" beats " + playerSelection +" !"
+                    cconsole.log("You Lose! "+ computerSelection+" beats " + playerSelection +" !");
+                    return "computer"
                     break;
             
                 default:
@@ -54,13 +65,16 @@ function playOneRound(playerSelection, computerSelection){
         case "scissors":
             switch (computerSelection) {
                 case "rock":
-                    return "You Lose! "+ computerSelection+" beats " + playerSelection +" !"
+                    console.log("You Lose! "+ computerSelection+" beats " + playerSelection +" !");
+                    return "computer"
                     break;
                 case "paper":
-                    return "You WIN!!! " + playerSelection +" beats " +  computerSelection+ " !!!"
+                    console.log("You WIN!!! " + playerSelection +" beats " +  computerSelection+ " !!!");
+                    return "player"
                     break;
                 case "scissors":
-                    return "There's a tie! Nobody wins!"
+                    console.log("It's a tie! Nobody wins!");
+                    return "tie"
                     break;
             
                 default:
@@ -73,4 +87,33 @@ function playOneRound(playerSelection, computerSelection){
     }
 }
 
-console.log(playOneRound(playerChoice, getComputerChoice()))
+
+function game(){
+    let rounds = 1;
+    let playerPoints = 0;
+    let computerPoints = 0;
+    for (let i = 0; i < 5; i++){
+        console.log(rounds);
+        let gameResult = playRound(choseOne(), getComputerChoice());
+        console.log(gameResult)
+
+        switch (gameResult) {
+            case "player":
+                playerPoints += 1;
+                break;
+
+            case "computer":
+                computerPoints += 1;
+                break;
+        
+            default:
+                break;
+        }
+
+        console.log("Round "+ rounds);
+        console.log("Player: " + playerPoints + " ---- Computer: " + computerPoints);
+        rounds++;
+    }
+}
+
+// game();
