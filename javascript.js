119ac4388efdbe91e1c449ac7144d30d33a32c31
Mariutti <pedromariutti@gmail.com>
2023-01-01 17:09:57 -0300

    Creating UI

--- javascript.js
+++ javascript.js
@@ -1,138 +1,151 @@
+const points = document.querySelector("#points");
+//creating a var to count the rounds
+let rounds = 1;
+//creating a var to count the player points
+let playerPoints = 0;
+//creating a ver to count the computer points
+let computerPoints = 0;
+
+const pOnePoints = document.querySelector("#pOnePoints");
+const pTwoPoints = document.querySelector("#pTwoPoints");
+pOnePoints.innerText = playerPoints;
+pTwoPoints.innerText = computerPoints;
+
+const paraResult = document.querySelector("#results");
+paraResult.innerText = "Resultado do jogo";
+
+const paraPoints = document.querySelector("#points");
+
+const btnRock = document.querySelector(".buttonRock");
+const btnPaper = document.querySelector(".buttonPaper");
+const btnScissors = document.querySelector(".buttonScissors");
+
+btnRock.addEventListener("click", () => {
+  let playerChoice = "rock";
+  game(playerChoice);
+});
+
+btnPaper.addEventListener("click", () => {
+  let playerChoice = "paper";
+  game(playerChoice);
+});
+
+btnScissors.addEventListener("click", () => {
+  let playerChoice = "scissors";
+  game(playerChoice);
+});
+
 // creating a function where the computer gets a choice to play the game
-function getComputerChoice(){
-    const gameOptions = ["rock", "paper", "scissors"]
-    return gameOptions[Math.floor(Math.random()*3)]
+function getComputerChoice() {
+  const gameOptions = ["rock", "paper", "scissors"];
+  return gameOptions[Math.floor(Math.random() * 3)];
 }
 
-
-// creating a var where we kept the player choice. I used a function to return the result:
-function choseOne() {
-    return window.prompt("rock, paper or scissors!").toLowerCase();
-}
-
+// creating a var where we kept the player choice:
+// function playerChoice() {
+//   return window.prompt("rock, paper or scissors!").toLowerCase();
+// }
 
 // creating a function to play a single round of the game
-function playRound(playerSelection, computerSelection){
-    console.log("Computer gets " + computerSelection)
-    console.log("You chose " + playerSelection);
-
-    //analyzing all results 
-    //showing a message with the round result
-    //returning a single word with the result
-    switch (playerSelection) {
-        case "rock":
-            switch (computerSelection) {
-                case "rock":
-                    console.log("It's a tie! Nobody wins!");
-                    return "tie"
-                    break;
-                case "paper":
-                    console.log("You Lose! "+ computerSelection+" beats " + playerSelection +" !");
-                    return "computer"
-                    break;
-                case "scissors":
-                    console.log("You WIN!!! " + playerSelection +" beats " +  computerSelection+ " !!!");
-                    return "player"
-                    break;
-            
-                default:
-                    break;
-            }
-            
-            break;
-        case "paper":
-            switch (computerSelection) {
-                case "rock":
-                    console.log("You WIN!!! " + playerSelection +" beats " +  computerSelection+ " !!!");
-                    return "player"
-                    break;
-                case "paper": 
-                    console.log("It's a tie! Nobody wins!");
-                    return "tie"
-                    break;
-                case "scissors":
-                    console.log("You Lose! "+ computerSelection+" beats " + playerSelection +" !");
-                    return "computer"
-                    break;
-            
-                default:
-                    break;
-            }
-                
-            break;  
-                
-        case "scissors":
-            switch (computerSelection) {
-                case "rock":
-                    console.log("You Lose! "+ computerSelection+" beats " + playerSelection +" !");
-                    return "computer"
-                    break;
-                case "paper":
-                    console.log("You WIN!!! " + playerSelection +" beats " +  computerSelection+ " !!!");
-                    return "player"
-                    break;
-                case "scissors":
-                    console.log("It's a tie! Nobody wins!");
-                    return "tie"
-                    break;
-            
-                default:
-                    break;
-            }            
-            break;                
-    
-        default:
-            break;
-    }
-}
-
-// creating a function to play a game with five round
-function game(){
-    //creating a var to count the rounds
-    let rounds = 1;
+function playRound(playerSelection, computerSelection) {
+  if (rounds > 5) {
+    rounds = 1;
     //creating a var to count the player points
-    let playerPoints = 0;
+    playerPoints = 0;
     //creating a ver to count the computer points
-    let computerPoints = 0;
+    computerPoints = 0;
+  }
+  paraResult.innerText = "You chose " + playerSelection;
+  paraResult.innerText += "\nComputer gets " + computerSelection;
 
-    // creating a loop of five rounds to play the function playRound
-    for (let i = 0; i < 5; i++){
-        // creating a var to keep the result o the function playRound()
-        let gameResult = playRound(choseOne(), getComputerChoice());
-        
-        //giving the points of the winner
-        switch (gameResult) {
-            case "player":
-                playerPoints += 1;
-                break;
-
-            case "computer":
-                computerPoints += 1;
-                break;
-        
-            default:
-                break;
-        }
-        //showing the round number and the result of rounds till there
-        console.log("Round "+ rounds +" results: Player: " + playerPoints + " ---- Computer: " + computerPoints);
-        rounds++;
-
-        //it's a five round game, so if someone gets 3 points, we have a winner and don't need to play to the end of rounds
-        if (playerPoints == 3){
-            return window.alert("You won 3 rounds and you're the winner!!!")
-        } else if (computerPoints == 3) {
-            return window.alert("Computer won 3 rounds, you lose the game!")
-        }
-    }
-
-    //if nobody gets 3 points till the end of loop, we will compare the results do declare the winner (or tie,)
-    if (playerPoints > computerPoints){
-        return window.alert("You won "+ playerPoints +" rounds and you're the winner!!!")
-    } else if (playerPoints < computerPoints){
-        return window.alert("Computer won "+ computerPoints +" rounds, you lose the game!")
-    } else {
-        return window.alert("It's a tie, nobody won.")
-    }
-
+  //analyzing all results to win - to lose - to tie
+  //showing a message with the round result
+  //returning a single word with the result
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    paraResult.innerText +=
+      "\nResultado do jogo: You WIN!!! " +
+      playerSelection +
+      " beats " +
+      computerSelection +
+      " !!!";
+    return "player";
+  } else if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "rock")
+  ) {
+    paraResult.innerText +=
+      "\nResultado do jogo: You Lose! " +
+      computerSelection +
+      " beats " +
+      playerSelection +
+      " !";
+    return "computer";
+  } else if (playerSelection == computerSelection) {
+    paraResult.innerText += "\nResultado do jogo: It's a tie! Nobody wins!";
+    return "tie";
+  }
 }
-game();
+// creating a function to play a game with five round
+function game(playerChoice) {
+  // creating a var to keep the result of the function playRound()
+  let gameResult = playRound(playerChoice, getComputerChoice());
+
+  //giving the points of the winner
+  switch (gameResult) {
+    case "player":
+      playerPoints += 1;
+      break;
+
+    case "computer":
+      computerPoints += 1;
+      break;
+
+    default:
+      break;
+  }
+  //showing the round number and the result of rounds till there
+  paraPoints.innerText =
+    "Round " +
+    rounds +
+    " results: Player: " +
+    playerPoints +
+    " ---- Computer: " +
+    computerPoints +
+    "\n";
+
+  pOnePoints.innerText = playerPoints;
+  pTwoPoints.innerText = computerPoints;
+  //it's a five round game, so if someone gets 3 points, we have a winner and don't need to play to the end of rounds
+  if (playerPoints == 3 && rounds >= 3) {
+    paraPoints.innerText += "You won 3 rounds and you're the winner!!!";
+    rounds = 0;
+    //creating a var to count the player points
+    playerPoints = 0;
+    //creating a ver to count the computer points
+    computerPoints = 0;
+  } else if (computerPoints == 3 && rounds >= 3) {
+    paraPoints.innerText += "Computer won 3 rounds, you lose the game!";
+    rounds = 0;
+    //creating a var to count the player points
+    playerPoints = 0;
+    //creating a ver to count the computer points
+    computerPoints = 0;
+  }
+
+  //if nobody gets 3 points till the end of loop, we will compare the results do declare the winner (or tie,)
+  if (rounds == 5 && playerPoints > computerPoints) {
+    paraPoints.innerText +=
+      "You won " + playerPoints + " rounds and you're the winner!!!";
+  } else if (rounds == 5 && playerPoints < computerPoints) {
+    paraPoints.innerText +=
+      "Computer won " + computerPoints + " rounds, you lose the game!";
+  } else if (rounds == 5 && playerPoints == computerPoints) {
+    paraPoints.innerText += "It's a tie, nobody won.";
+  }
+  rounds++;
+}
